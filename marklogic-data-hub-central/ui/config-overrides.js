7caeaaff4a546adb2b0096aba7f827a29ced2e1e
Bruce An <Bruce.An@marklogic.com>
2020-06-10 14:42:30 -0700

    UX-129: Design System build and test failures

--- marklogic-data-hub-central/ui/config-overrides.js
+++ marklogic-data-hub-central/ui/config-overrides.js
@@ -3,10 +3,28 @@ const path = require('path')
 const themeVariables = require('@marklogic/design-system/src/theme-variables.json')
 
 module.exports = override(
-  babelInclude([
+    babelInclude([
     path.resolve(__dirname, 'src'),
-    /@marklogic\/design-system/
   ]),
+  fixBabelImports('@marklogic/design-system/es/MLIcon',
+    {
+      libraryDirectory: '',
+      camel2DashComponentName: false,
+      customName: function (name) {
+        return `@marklogic/design-system/es/MLIcon/${name}`
+      },
+      style: function() {
+        return '@marklogic/design-system/es/MLIcon/style'
+      },
+    },
+  ),
+  fixBabelImports('@marklogic/design-system',
+    {
+      libraryDirectory: 'es',
+      camel2DashComponentName: false,
+      style: true,
+    },
+  ),
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
